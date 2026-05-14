4e4fe7e0001a9628b90ec8f65f9018c4a14bc7d2
Alexander Liu <alexander.liu22@outlook.com>
2026-05-14 13:46:44 -0700

    [1914][medium] add slightly optimized soln

--- 1914-[MEDIUM]-Cyclically-Rotating-a-Grid.py
+++ 1914-[MEDIUM]-Cyclically-Rotating-a-Grid.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     """
     Intuition:
         To compute the number of layers, we need to take the length of the
@@ -64,5 +64,92 @@ class Solution:
                 destX, destY = coords[destIx]
                 # store destination value in tmp and shift curr value to it
                 grid[destX][destY] = vals[i]
+
+        return grid
+
+
+class Solution2:
+    """
+    Intuition:
+        Same general intuition as Solution 1 with some optimizations.
+
+        Remove need to auxiliary memory to store coordinates. We can just
+        traverse using same logic we build the 'vals' array storing values
+        to populate array with rotated values.
+
+        We compute the shift (k % L) in case k is very, very large. This
+        avoids costly arithmetic.
+
+        Pre-rotate all values to facilitate traversal of values when
+        rotating a layer.
+
+    Runtime:
+        Same as Solution 1.
+
+    Memory:
+        Same as Solution 1.
+    """
+
+    def rotateGrid(self, grid: list[list[int]], k: int) -> list[list[int]]:
+        if k == 0:
+            return grid
+
+        M, N = len(grid), len(grid[0])
+        layers = min(M, N) // 2
+
+        # perform rotations per layer
+        for layer in range(layers):
+            # gather vals in counter clockwise order
+            vals = []
+
+            # go down
+            col = layer
+            for row in range(layer, M - layer):
+                vals.append(grid[row][col])
+
+            # go right
+            row = M - layer - 1
+            for col in range(layer + 1, N - layer):
+                vals.append(grid[row][col])
+
+            # go up
+            col = N - layer - 1
+            for row in range(M - layer - 2, layer - 1, -1):
+                vals.append(grid[row][col])
+
+            # go left
+            row = layer
+            for col in range(N - layer - 2, layer, -1):
+                vals.append(grid[row][col])
+
+            # rearrange elmts
+            L = len(vals)
+            shift = k % L  # in case k is very large
+            rotated = vals[-shift:] + vals[:-shift]
+            ix = 0
+
+            # go down
+            col = layer
+            for row in range(layer, M - layer):
+                grid[row][col] = rotated[ix]
+                ix += 1
+
+            # go right
+            row = M - layer - 1
+            for col in range(layer + 1, N - layer):
+                grid[row][col] = rotated[ix]
+                ix += 1
+
+            # go up
+            col = N - layer - 1
+            for row in range(M - layer - 2, layer - 1, -1):
+                grid[row][col] = rotated[ix]
+                ix += 1
+
+            # go left
+            row = layer
+            for col in range(N - layer - 2, layer, -1):
+                grid[row][col] = rotated[ix]
+                ix += 1
 
         return grid
