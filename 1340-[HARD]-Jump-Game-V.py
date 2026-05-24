90fff80a48f3c11b11a380b9c5e3e2175b0df98f
Alexander Liu <alexander.liu22@outlook.com>
2026-05-23 22:27:00 -0700

    [1340][hard] add bottom-up DP soln

--- 1340-[HARD]-Jump-Game-V.py
+++ 1340-[HARD]-Jump-Game-V.py
@@ -1,7 +1,7 @@
 from math import inf
 
 
-class Solution:
+class Solution1:
     """
     Intuition:
         We use a top-down DP approach and store the current position
@@ -74,3 +74,60 @@ class Solution:
             res = max(res, dp(i))
 
         return res
+
+
+class Solution2:
+    """
+    Intuition:
+        Bottom-up DP solution to avoid overhead incurred by recursion.
+
+        We sort the elements of the input array by value because by
+        processing the indices in ascending order of their respective
+        values, we ensure that smaller neighbours have been computed
+        for the current index/value pair we are on. Thus, we can simply
+        perform a O(1) lookup to our DP cache to figure out the current
+        solution.
+
+    Runtime:
+        Sorting takes O(n * log n).
+
+        Iterating through the sorted pairs and scanning the neighbours
+        we can jump to still takes O(n * d).
+
+        Finding the maximum value in our DP cache takes O(n) time.
+
+        Overall, we have a runtime complexity bounded by O(n * d) unless
+        d <<< n in which case O(n * log n) dominates.
+
+    Memory:
+        O(n) for the DP cache.
+    """
+
+    def maxJumps(self, arr: list[int], d: int) -> int:
+        N = len(arr)
+        # base case -- smallest value has nowhere to jump which is why we skip 1st elmt
+        dp = [1] * N
+
+        # sort (ix, val) tuples by ascending value
+        pairs = sorted(enumerate(arr), key=lambda x: x[1])
+
+        for ix, currVal in pairs[1:]:
+            currRes = 1
+            l, r = max(0, ix - d), min(N - 1, ix + d)
+            # scan left
+            for j in range(ix - 1, l - 1, -1):
+                if arr[j] >= currVal:
+                    break
+
+                currRes = max(currRes, dp[j] + 1)
+
+            # scan right
+            for j in range(ix + 1, r + 1):
+                if arr[j] >= currVal:
+                    break
+
+                currRes = max(currRes, dp[j] + 1)
+
+            dp[ix] = currRes
+
+        return max(dp)
