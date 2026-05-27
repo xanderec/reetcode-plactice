9a14dc1e72fcca29987e202904dd97277dff1a70
xanderec <alexander.liu22@outlook.com>
2026-05-26 17:36:59 -0700

    [3121][medium] add brute force soln

---
+++ 3121-[MEDIUM]-Count-the-Number-of-Special-Characters-II.py
@@ -0,0 +1,44 @@
+class Solution:
+    """
+    Intuition:
+        Brute force approahc. Linearly scan the input word and use
+        bitmaps to track state. State transitions are a bit messy,
+        but the idea is to transition the 'upper' bitmap based on
+        if we have encountered a lowercase variant of that letter.
+        The lowercase letters can also be invalidated when we
+        encounter a lower letter after its upper variant or if we
+        encounter the lower variant once the letter was previously
+        invalidated.
+
+    Runtime:
+        O(n), one pass.
+
+    Memory:
+        O(1).
+    """
+
+    def numberOfSpecialChars(self, word: str) -> int:
+        # 0 = initial state, 1 = encountered, -1 = invalid
+        lower = [0] * 26
+        upper = [0] * 26
+
+        for c in word:
+            ix = ord(c.lower()) - ord("a")
+
+            if c.islower():
+                # happy path -- encounter lower
+                if lower[ix] == 0:
+                    lower[ix] = 1
+                # already encountered uppercase, invalidate char
+                elif upper[ix] == 1:
+                    lower[ix] = -1
+                    upper[ix] = 0
+            else:  # upper case char
+                # happy path -- lower encountered before upper
+                if lower[ix] == 1:
+                    upper[ix] = 1
+                # either char is invalid or no lower instance encountered
+                else:
+                    lower[ix] = -1
+
+        return sum(upper)
