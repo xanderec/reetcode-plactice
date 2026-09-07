17b41c0ebbdecfb4f9eedb56915829419019c39e
Alexander Liu <alexander.liu22@outlook.com>
2026-09-06 23:13:31 -0400

    [115][hard] add bottom-up tabulation soln

--- 0115-[HARD]-Distinct-Subsequences.py
+++ 0115-[HARD]-Distinct-Subsequences.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     """
     Intuition:
         Use a top-down memoization approach.
@@ -53,3 +53,59 @@ class Solution:
 
         dp(0, 0)
         return cache[0][0]
+
+
+class Solution2:
+    """
+    Intuition:
+        Bottom-up tabulation approach. Turns our heavy recursive soln
+        into a fast iterative soln.
+
+        Let the function F(i, j) output the number of distinct subseqs
+        equal to t[:j] we can form using prefix s[:i]. To solve for that,
+        we need to consider how many distinct subseqs we can form using
+        s[:i - 1] i.e. F(i - 1, j). If s[i] == t[j], then we also have
+        to consider how many distinct subseqs equal to t[:j - 1] we can
+        form using s[:i - 1].
+
+        Thus, the recurrence relation becomes:
+
+            F(i, j) = F(i - 1, j) if s[i] != t[j]
+
+            F(i, j) = F(i - 1, j) + F(i - 1, j - 1) if s[i] == t[j].
+
+        Notice how the recurrence relation only depends on state i - 1 at
+        state i. Since we only ever depend on the previous row, we can
+        collapse the need for a 2D cache to 1D. The cache simply represents
+        the current row of state.
+
+    Runtime:
+        Still O(m * n).
+
+    Memory:
+        O(n) for the cache.
+    """
+
+    def numDistinct(self, s: str, t: str) -> int:
+        M, N = len(s), len(t)
+        # early exit
+        if N > M:
+            return 0
+
+        # base case at ix=0, only 1 way to form empty str i.e. len 0
+        #
+        # dp table answers: how many distinct subseqs equal to t[:j]
+        # can we form using prefix s[:i]
+        dp = [1] + [0] * N
+
+        for i in range(1, M + 1):
+            # call to min() is to cap when i is small, we haven't
+            # explored N chars yet in s
+            #
+            # we iterate backwards to prevent double counting when
+            # transitioning to next row of state
+            for j in range(min(i, N), 0, -1):
+                if s[i - 1] == t[j - 1]:
+                    dp[j] += dp[j - 1]
+
+        return dp[N]
